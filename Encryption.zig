a3e05e4438b36e9201d77aa0b051dc210b93db45
Soroosh Chabi <soroosh.chabi@gmail.com>
2025-06-08 17:49:46 +0330

    Organize Encryption tests as doc tests

--- Encryption.zig
+++ Encryption.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const Encryption = @This();
+
 pub const overhead = cipher.nonce_length;
 pub const key_metadata_length = salt_length + overhead + correct_password.len;
 pub const KeyLoadError = error{
@@ -11,7 +13,7 @@ const salt_length = 64;
 key: [cipher.key_length]u8 = undefined,
 
 /// `dst` must be bigger than `src` by `overhead`
-pub fn encrypt(self: @This(), src: []const u8, dst: []u8) void {
+pub fn encrypt(self: Encryption, src: []const u8, dst: []u8) void {
     var nonce: [cipher.nonce_length]u8 = undefined;
     std.crypto.random.bytes(&nonce);
     @memcpy(dst[0..overhead], &nonce);
@@ -19,13 +21,13 @@ pub fn encrypt(self: @This(), src: []const u8, dst: []u8) void {
 }
 
 /// `src` must be bigger than `dst` by `overhead`
-pub fn decrypt(self: @This(), src: []const u8, dst: []u8) void {
+pub fn decrypt(self: Encryption, src: []const u8, dst: []u8) void {
     var nonce: [cipher.nonce_length]u8 = undefined;
     @memcpy(&nonce, src[0..overhead]);
     cipher.xor(dst, src[overhead..], 0, self.key, nonce);
 }
 
-pub fn loadKeyMetadata(self: *@This(), password: []const u8, key_metadata: [key_metadata_length]u8) KeyLoadError!void {
+pub fn loadKeyMetadata(self: *Encryption, password: []const u8, key_metadata: [key_metadata_length]u8) KeyLoadError!void {
     self.setKey(password, key_metadata[0..salt_length]);
     var decrypted_metadata: [correct_password.len]u8 = undefined;
     self.decrypt(key_metadata[salt_length..], &decrypted_metadata);
@@ -35,48 +37,42 @@ pub fn loadKeyMetadata(self: *@This(), password: []const u8, key_metadata: [key_
 }
 
 /// `key_metadata` should be `key_metadata_length` long
-pub fn generateKeyMetadata(self: *@This(), password: []const u8, key_metadata: *[key_metadata_length]u8) void {
+pub fn generateKeyMetadata(self: *Encryption, password: []const u8, key_metadata: *[key_metadata_length]u8) void {
     const salt = key_metadata[0..salt_length];
     std.crypto.random.bytes(salt);
     self.setKey(password, salt);
     self.encrypt(correct_password, key_metadata[salt_length..]);
 }
 
-fn setKey(self: *@This(), password: []const u8, salt: []const u8) void {
+fn setKey(self: *Encryption, password: []const u8, salt: []const u8) void {
     const prk = std.crypto.kdf.hkdf.HkdfSha512.extract(salt, password);
     std.crypto.kdf.hkdf.HkdfSha512.expand(&self.key, &[0]u8{}, prk);
 }
 
-test "generate, encrypt and decrypt correctly" {
-    const plain_buffer = "hello";
-    var cipher_buffer: [plain_buffer.len + overhead]u8 = undefined;
-    var decrypted_buffer: [plain_buffer.len]u8 = undefined;
-    var crypto = @This(){};
+test Encryption {
+    var enc = Encryption{};
     var key_metadata: [key_metadata_length]u8 = undefined;
-    crypto.generateKeyMetadata("mousy", &key_metadata);
-    crypto.encrypt(plain_buffer, &cipher_buffer);
-    crypto.decrypt(&cipher_buffer, &decrypted_buffer);
-    try std.testing.expectEqualStrings(plain_buffer, &decrypted_buffer);
-}
-
-test "generate and load key metadata correctly" {
-    var key_metadata: [key_metadata_length]u8 = undefined;
-    var enc_gen = @This(){};
-    enc_gen.generateKeyMetadata("mousy", &key_metadata);
-    const plain_buffer = "hello";
-    var cipher_buffer: [plain_buffer.len + overhead]u8 = undefined;
-    enc_gen.encrypt(plain_buffer, &cipher_buffer);
-    var enc_load = @This(){};
-    try enc_load.loadKeyMetadata("mousy", key_metadata);
-    var decrypted_buffer: [plain_buffer.len]u8 = undefined;
-    enc_load.decrypt(&cipher_buffer, &decrypted_buffer);
-    try std.testing.expectEqualStrings(plain_buffer, &decrypted_buffer);
-}
-
-test "load wrong key metadata" {
-    var key_metadata: [key_metadata_length]u8 = undefined;
-    var enc_gen = @This(){};
-    enc_gen.generateKeyMetadata("mousy", &key_metadata);
-    var enc_load = @This(){};
-    try std.testing.expectError(KeyLoadError.WrongPassword, enc_load.loadKeyMetadata("catty", key_metadata));
+    const password = "password";
+    enc.generateKeyMetadata(password, &key_metadata);
+    {
+        const plain_buffer = "plain";
+        var cipher_buffer: [plain_buffer.len + overhead]u8 = undefined;
+        enc.encrypt(plain_buffer, &cipher_buffer);
+        {
+            var decrypted_buffer: [plain_buffer.len]u8 = undefined;
+            enc.decrypt(&cipher_buffer, &decrypted_buffer);
+            try std.testing.expectEqualStrings(plain_buffer, &decrypted_buffer);
+        }
+        {
+            var enc_load = Encryption{};
+            try enc_load.loadKeyMetadata(password, key_metadata);
+            var decrypted_buffer: [plain_buffer.len]u8 = undefined;
+            enc_load.decrypt(&cipher_buffer, &decrypted_buffer);
+            try std.testing.expectEqualStrings(plain_buffer, &decrypted_buffer);
+        }
+    }
+    {
+        var enc_load = Encryption{};
+        try std.testing.expectError(KeyLoadError.WrongPassword, enc_load.loadKeyMetadata("catty", key_metadata));
+    }
 }
